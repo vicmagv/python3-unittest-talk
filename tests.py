1bd9f6047f45a0147eddf678a04539482f689034
vicmagv <vicmagv@gmail.com>
2015-05-04 11:33:43 +0200

    slides and tests.py finished

--- tests.py
+++ tests.py
@@ -2,21 +2,25 @@ import unittest
 from example import *
 
 class TestClass1(unittest.TestCase): #test case
+    a = "asdlfkjasdlkfjalsdfña"
+
+    def setUp(self):
+        print('comienza el test! :)')
+
+    def tearDown(self):
+        print('acaba el test :(')
 
     def test_all_empty(self):
         self.assertTrue( characters_not_included("","") ==  "" )
 
     def test_same_words(self):
-        a = "asdlfkjasdlkfjalsdfña"
-        self.assertTrue( characters_not_included(a,a) == "" )
+        self.assertTrue( characters_not_included(self.a,self.a) == "" )
 
     def test_first_empty(self):
-        a = "asdlfkjasdlkfjalsdfña"
-        self.assertTrue( characters_not_included("",a) == "")
+        self.assertTrue( characters_not_included("",self.a) == "")
 
     def test_second_empty(self):
-        a = "asdlfkjasdlkfjalsdfña"
-        self.assertTrue( set(characters_not_included(a,"")) == set(a) )
+        self.assertTrue( set(characters_not_included(self.a,"")) == set(self.a) )
 
 
 if __name__ == '__main__':
