9d78064a967e64938a59e33b0289591dc28fc69f
vicmagv <vicmagv@gmail.com>
2015-05-04 10:28:07 +0200

    slides added, Readme modified

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python_unittest_talk
-Talk about python unittest 3.4.3 for Software Engineering Tools subject at University of Almería.
+Talk about python unittest 3.4.3 for Software Engineering: Tools and Methods subject at University of Almería.
+
+Slides are in Spanish for academic purposes.
--- example.py
+++ example.py
@@ -1,10 +1,5 @@
 #HMIS demo unitest
 
-# Ejercicio 5
-# Escriba el código de un sencillo método o función que tome cómo parámetros
-# de entrada dos cadenas de caracteres, P1, P2, y devuelva otra cadena que
-# contenga los caracteres de P1 que no están en P2.
-
 #Returns the characters of 'string1' not included in 'string2'
 def characters_not_included(p1, p2):
     return "".join(set(p1).difference(set(p2)))
